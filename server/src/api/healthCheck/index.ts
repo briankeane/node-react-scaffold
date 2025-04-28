9b39588d2c602411867373beeb8d39b2a4cccaaf
Brian Keane <lonesomewhistle@gmail.com>
2025-04-28 12:55:19 -0500

    healtcheck endpoint working

--- server/src/api/healthCheck/index.ts
+++ server/src/api/healthCheck/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import controller from './healthCheck.api';
 
-console.log('hi;0');
 const router = express.Router();
 
 router.get('/', controller.healthCheckEndpoint);
+
+export default router;
--- server/src/api/routes.ts
+++ server/src/api/routes.ts
@@ -1,7 +1,8 @@
 import { Application } from 'express';
+import healthCheckApi from './healthCheck';
 
 function addRoutes(app: Application) {
-  app.use('/v1/healthCheck', require('./healthCheck'));
+  app.use('/v1/healthCheck', healthCheckApi);
 }
 
 export default addRoutes;
