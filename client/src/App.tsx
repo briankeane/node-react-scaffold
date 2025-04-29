6f81c0c28a108e5fa88a7bbe7eede5aad194351d
Brian Keane <lonesomewhistle@gmail.com>
2025-04-28 20:26:02 -0500

    working tests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg';
 import Counter from './Components/Counter';
 import viteLogo from '/vite.svg';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
