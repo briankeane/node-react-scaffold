5ad7ddbc1d0856c469cc1e2b08e1daf7b348abca
Brian Keane <lonesomewhistle@gmail.com>
2026-03-01 13:01:45 -0600

    test infrastructure

--- server/src/test/mochaSetup.test.ts
+++ server/src/test/mochaSetup.test.ts
@@ -1,20 +1,30 @@
-/*
- * Global setup code for mocha tests
- * note: I think this works (executes first) because
- * it is not inside of a describe block.   It still must
- * stay within the '${ROOT}/test' folder.
- */
-
-import { afterEach, before } from "mocha";
-// import db from '../db';
+import { after, afterEach, before } from "mocha";
+import nock from "nock";
+import { performance } from "node:perf_hooks";
 import app from "../server";
+import { clearDatabase } from "./test.helpers";
+
+const testRunStart = performance.now();
+
+nock.disableNetConnect();
+nock.enableNetConnect("127.0.0.1");
 
 before(async function (this: Mocha.Context) {
   this.timeout(5000);
   await app.isReadyPromise;
-  // await clearDatabase(db);
+  await clearDatabase();
 });
 
 afterEach(async function () {
-  // await clearDatabase(db);
+  await clearDatabase();
+});
+
+after(function () {
+  nock.cleanAll();
+  nock.restore();
+
+  const durationSeconds = ((performance.now() - testRunStart) / 1000).toFixed(
+    2,
+  );
+  console.log(`Total test duration: ${durationSeconds}s`);
 });
